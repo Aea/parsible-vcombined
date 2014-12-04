9bec0997faaa2c978be4a47779b0ac4bbcb86828
Artur Ergashev <artur@ergashev.com>
2014-12-03 23:10:15 -0700

    83ebebe3b443b232f83c98ee6f8a75c2e460b61f Part #2

--- plugins/processors/text.py
+++ plugins/processors/text.py
@@ -105,7 +105,7 @@ def process_html_paths(data):
         fp.close()
     
     # Skip non-html
-    if "text/html" in data.get("content_type", ""):
+    if "text/html" not in data.get("content_type", ""):
         return
     
     # Skip Invalid Codes
